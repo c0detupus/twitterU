e01f977cb52ac5bfa111dc4ab8b8fa1dad02cc06
c0detupus <alexander.mehtala@gmail.com>
2015-10-20 19:35:23 +0200

    Staggered grid view now has dummy list

--- app/src/main/java/com/afk/twitteru/fragments/StaggeredGridFragment.java
+++ app/src/main/java/com/afk/twitteru/fragments/StaggeredGridFragment.java
@@ -9,13 +9,60 @@ import android.view.ViewGroup;
 
 
 import com.afk.twitteru.R;
+import com.afk.twitteru.developer.Tweet;
+import com.afk.twitteru.developer.TweetListAdapter;
+import com.etsy.android.grid.StaggeredGridView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class StaggeredGridFragment extends Fragment {
+
+    private StaggeredGridView staggeredGridView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_staggered_grid, container, false);
+    }
+
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        staggeredGridView = (StaggeredGridView) getActivity().findViewById(R.id.grid_view);
+
+
+        List<Tweet> tweets = new ArrayList<>();
+        tweets.add(new Tweet("Hej"));
+        tweets.add(new Tweet("what"));
+        tweets.add(new Tweet("are"));
+        tweets.add(new Tweet("you doing here you are not supposed to be here"));
+        tweets.add(new Tweet("What evs"));
+        tweets.add(new Tweet("Shah"));
+        tweets.add(new Tweet("are"));
+        tweets.add(new Tweet("you doing here you are not supposed to be here"));
+        tweets.add(new Tweet("What evs"));
+        tweets.add(new Tweet("Shah"));   tweets.add(new Tweet("are"));
+        tweets.add(new Tweet("you doing here you are not supposed to be here"));
+        tweets.add(new Tweet("What evs"));
+        tweets.add(new Tweet("Shah"));   tweets.add(new Tweet("are"));
+        tweets.add(new Tweet("you doing here you are not supposed to be here"));
+        tweets.add(new Tweet("What evs"));
+        tweets.add(new Tweet("Shah"));   tweets.add(new Tweet("are"));
+        tweets.add(new Tweet("you doing here you are not supposed to be here"));
+        tweets.add(new Tweet("What evs"));
+        tweets.add(new Tweet("Shah"));   tweets.add(new Tweet("are"));
+        tweets.add(new Tweet("you doing here you are not supposed to be here"));
+        tweets.add(new Tweet("What evs"));
+        tweets.add(new Tweet("Shah"));   tweets.add(new Tweet("are"));
+        tweets.add(new Tweet("you doing here you are not supposed to be here"));
+        tweets.add(new Tweet("What evs"));
+        tweets.add(new Tweet("Shah"));
+
+        staggeredGridView.setAdapter(new TweetListAdapter(getActivity().getApplicationContext(), tweets));
+
     }
 }
